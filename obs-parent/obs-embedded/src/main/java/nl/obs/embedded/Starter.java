3ab29f6b1101beea909dabcde29b8a0e453f8992
Bas Verburg <b.verburg@codecast.nl>
2012-08-02 22:56:09 +0200

    added embedded tomcat for easy testing and deployment

---
+++ obs-parent/obs-embedded/src/main/java/nl/obs/embedded/Starter.java
@@ -0,0 +1,36 @@
+package nl.obs.embedded;
+
+import java.io.File;
+
+
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starter {
+	private final static Logger logger = LoggerFactory.getLogger(Starter.class);
+	
+	private final static String LOCATION = "src/main/webapp/";
+
+	public static void main(String[] args) throws Exception {
+
+        
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.addWebapp("/", new File(LOCATION).getAbsolutePath());
+        logger.info("configuring app with basedir: {}" , new File("./" + LOCATION).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();  
+    }
+
+}
