06dbe4f711dd2d9147cc32215984692260f3e111
Bas Verburg <b.verburg@codecast.nl>
2012-08-02 21:37:37 +0200

    added logging and initial commit of testrunner

---
+++ obs-parent/obs-core/src/test/java/nl/obs/core/db/TestRunner.java
@@ -0,0 +1,18 @@
+package nl.obs.core.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TestRunner {
+
+	static Logger logger = LoggerFactory.getLogger(TestRunner.class);
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.info("Starting testrunner");
+	}
+
+}
