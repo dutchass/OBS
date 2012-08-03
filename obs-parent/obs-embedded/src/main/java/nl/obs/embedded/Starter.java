7850062101cac791ae38ed334763fa49dea642c2
Bas Verburg <b.verburg@codecast.nl>
2012-08-03 12:31:41 +0200

    added test servlets and made embedded run, still has abs path though

--- obs-parent/obs-embedded/src/main/java/nl/obs/embedded/Starter.java
+++ obs-parent/obs-embedded/src/main/java/nl/obs/embedded/Starter.java
@@ -2,7 +2,7 @@ package nl.obs.embedded;
 
 import java.io.File;
 
-
+import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,10 +10,13 @@ import org.slf4j.LoggerFactory;
 public class Starter {
 	private final static Logger logger = LoggerFactory.getLogger(Starter.class);
 	
-	private final static String LOCATION = "src/main/webapp/";
+	private final static String LOCATION = "C:\\Users\\bas\\workspace3\\OBS\\obs-parent\\obs-embedded\\target\\dependency\\obs-web-0.1.0-SNAPSHOT.war";
 
 	public static void main(String[] args) throws Exception {
 
+		for (String string : args) {
+			logger.info("arg: "+ string);
+		}
         
         Tomcat tomcat = new Tomcat();
 
@@ -25,9 +28,18 @@ public class Starter {
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
+        
+        StandardHost stdHost = (StandardHost) tomcat.getHost();
+        stdHost.setUnpackWARs(false); 
+        stdHost.setAutoDeploy(true); 
+        stdHost.setDeployOnStartup(true); 
+
+        tomcat.setHost(stdHost); 
 
         tomcat.addWebapp("/", new File(LOCATION).getAbsolutePath());
-        logger.info("configuring app with basedir: {}" , new File("./" + LOCATION).getAbsolutePath());
+        logger.info("configuring app with basedir: {}" , new File(LOCATION).getAbsolutePath());
+        
+        
 
         tomcat.start();
         tomcat.getServer().await();  
--- obs-parent/obs-web/src/main/java/nl/obs/web/servlet/HelloServlet.java
+++ obs-parent/obs-web/src/main/java/nl/obs/web/servlet/HelloServlet.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(
+        name = "HelloServlet", 
+        urlPatterns = {"/hello"}
+    )
 public class HelloServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6663501646932303333L;
-
+	
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
